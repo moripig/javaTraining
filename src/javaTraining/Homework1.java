9c40982348c0409088ea35cb1891f13be23b1940
moripig <moripig@DESKTOP-354MD4O>
2019-09-15 23:28:47 +0900

    조건문 추가 및 완성

--- src/javaTraining/Homework1.java
+++ src/javaTraining/Homework1.java
@@ -9,18 +9,28 @@ public class Homework1 {
 	public static void main(String[] args) {
 
 	Scanner scan = new Scanner(System.in);
-		int count = 0;
+		int count = 1;
 	    String word;
 	        
 	    System.out.println("문자열 입력 : ");
 	    word = scan.nextLine();        
 	       
 	    for(int i = 0; i < word.length(); i++) { // 문자열에 공백이 있을때마다 count가 1 증가
-	    	if(word.charAt(i) == ' ') {
+	    	if(word.charAt(i) == ' ') { 
 	    		count++;                
 	        }
 	    }
+	    
+	    if(word.charAt(0) == ' ') { // 문자열 처음에 공백이 있을 경우 
+	    	count--;
+	    }
+	    
+	    if(word.charAt(word.length()-1) == ' ') { // 문자열 마지막이 공백으로 끝날 경우
+	    	count--;
+	    }
+	    
 	    System.out.println(count);
 	}
-
 }
+
+
